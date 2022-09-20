3d4329e3b73cbd8cba6563732f5adf0f83e60f88
Souvik Das <dassouvik180@gmail.com>
2022-09-20 17:12:08 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,235 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")                // requiring mongoose
+const bodyParser = require("body-parser");          // external npm module
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+//const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+
+ 
+// const currentDay = require(__dirname + "/date.js"); // We are requiring the module we created in date.js,which binds all of the exports to the const calld date
+const _ = require("lodash");
+const alert = require('alert');
+
+
+
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+
+
+app.set( 'view engine' , 'ejs');
+
+app.use(bodyParser.urlencoded({ extended : true }));
+
+app.use(express.static("Public"));
+
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+  }));
+  
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+
+
+mongoose.connect("mongodb://localhost:27017/memeUserDB", {useNewUrlParser: true , useUnifiedTopology: true});
+//mongoose.set("useCreateIndex", true);
+
+
+const userSchema = mongoose.Schema({
+    email : String,
+    password : String
+});
+
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+
+const User = mongoose.model('user' , userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+const postSchema = {
+
+  title: String,
+  content: String
+ 
+ };
+ 
+ const Post = mongoose.model("Post", postSchema);
+
+
+app.get("/", function(req, res){
+    res.render("home");
+  });
+
+
+app.get("/register" , function(req , res){
+    res.render("register");
+});
+
+app.get("/blog-home" , function(req , res) {
+   
+  if(req.isAuthenticated()){
+
+      Post.find({}, function(err, posts){
+    
+         res.render("blog-home", {
+    
+           startingContent: homeStartingContent,
+    
+           posts: posts
+    
+           });
+    
+       })
+    
+  } else {
+    alert("Kindly Sign in or Register to proceed")
+
+    res.redirect("/");
+  }
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+
+app.get("/details" , function(req , res){
+  res.render("details" , {aboutContent: aboutContent} )
+});
+
+app.get("/compose", function(req, res){
+  
+  if(req.isAuthenticated()){
+    res.render("compose");
+  } else {
+    alert("Kindly Sign in or Register to Compose")
+
+    res.redirect("/");
+  }
+  
+  
+});
+
+app.get("/posts/:postId", function(req, res){
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({_id : requestedPostId}, function(err, post){
+
+    res.render("post", {
+
+       title: post.title,
+
+       content: post.content
+
+    });
+
+  });
+});
+
+
+
+app.get("/logout", function(req, res){
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+
+app.post("/" , (req , res)=>{
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/blog-home");
+      });
+    }
+  });
+
+    });
+
+app.post("/register", (req , res)=>{
+    console.log(req.body);
+
+    
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/blog-home");
+      });
+    }
+  });
+  
+})
+
+
+app.post("/compose", function(req, res){
+     const post = new Post ({
+       title: req.body.postTitle,
+       content: req.body.postBody
+      });
+
+   post.save(function(err){
+      if (!err){
+      res.redirect("/blog-home");
+    }
+
+  });
+});
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000.");
+  });
+
+
+
